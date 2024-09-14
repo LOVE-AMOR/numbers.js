e706e571c9e81b1a87ec046a2c6e3787dd6ca629
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-09-13 20:24:14 -0800

    Iniial commit

---
+++ numbers.js
@@ -0,0 +1,20 @@
+let firstFavNum = 16;
+let secondFavNum = 10;
+
+let addition = firstFavNum + secondFavNum;
+console.log("Addition:", addition);
+
+let subtraction = firstFavNum - secondFavNum;
+console.log("Subtraction:", subtraction);
+
+let multiplication = firstFavNum * secondFavNum;
+console.log("Multiplication:", multiplication);
+
+let division = firstFavNum / secondFavNum;
+console.log("Division:", division);
+
+let modulus = firstFavNum % secondFavNum;
+console.log("Modulus:", modulus);
+
+let exponentiation = Math.pow(firstFavNum, secondFavNum);
+console.log("Exponentiation:", exponentiation);
